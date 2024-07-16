de8041c67939417e840629befc1a3df23fc6ce5f
glen <glenwojnar@yahoo.com>
2024-07-15 20:40:22 -0600

    Started queues

---
+++ AandDSforBeginners/LinkedLists/Queues/NumberOfStudentsUnableToEatLunch.py
@@ -0,0 +1,14 @@
+import collections
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        res = len(students)
+        count = collections.Counter(students)
+
+        for s in sandwiches:
+            if count[s] > 0:
+                res -= 1
+                count[s] -= 1
+            else:
+                return res
+
+        return res
