731d2fb51425a3c074245d1f4c54bf8859b573e7
glen <glenwojnar@yahoo.com>
2024-09-16 22:58:57 -0600

    faster solve

--- AandDSforBeginners/Sorting/QuickSort/QuickSort.py
+++ AandDSforBeginners/Sorting/QuickSort/QuickSort.py
@@ -1,3 +1,4 @@
+# Slower
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         self.quickSortHelper(nums, 0, (len(nums) - 1))
@@ -20,3 +21,50 @@ class Solution:
 
         self.quickSortHelper(arr, s, left - 1)
         self.quickSortHelper(arr, left + 1, e)
+
+
+
+# Faster
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        self.quickSortHelper(nums, 0, len(nums) - 1)
+        return nums
+
+    def quickSortHelper(self, arr, s, e):
+        if s >= e:
+            return
+
+        # Use median-of-three pivot selection
+        pivot_index = self.medianOfThree(arr, s, e)
+        pivot = arr[pivot_index]
+        arr[pivot_index], arr[e] = arr[e], arr[pivot_index]
+
+        # Partitioning
+        left = s
+        for i in range(s, e):
+            if arr[i] < pivot:
+                arr[left], arr[i] = arr[i], arr[left]
+                left += 1
+        arr[left], arr[e] = arr[e], arr[left]
+
+        # Recursively apply to sub-arrays
+        self.quickSortHelper(arr, s, left - 1)
+        self.quickSortHelper(arr, left + 1, e)
+
+    def medianOfThree(self, arr, s, e):
+        mid = (s + e) // 2
+        a, b, c = arr[s], arr[mid], arr[e]
+        if a < b:
+            if b < c:
+                return mid
+            elif a < c:
+                return e
+            else:
+                return s
+        else:
+            if a < c:
+                return s
+            elif b < c:
+                return e
+            else:
+                return mid
