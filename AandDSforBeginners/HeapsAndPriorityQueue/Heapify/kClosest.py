a80b63388b0d895c55b0197c976e8b095c68cc12
glen <glenwojnar@yahoo.com>
2024-11-19 19:51:23 -0700

    Heaps done

--- AandDSforBeginners/HeapsAndPriorityQueue/Heapify/kClosest.py
+++ AandDSforBeginners/HeapsAndPriorityQueue/Heapify/kClosest.py
@@ -15,3 +15,7 @@ class Solution:
             res.append(curr[1])
 
         return res
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        points.sort(key=lambda p: p[0]**2 + p[1]**2)
+        return points[:k]
