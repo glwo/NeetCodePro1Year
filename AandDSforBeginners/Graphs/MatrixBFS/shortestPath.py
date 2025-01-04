db36a7027c31a2ccdb5131b03fa4b0f34a5a6a27
glen <glenwojnar@yahoo.com>
2025-01-03 22:33:17 -0700

    readable solution for shortest path

--- AandDSforBeginners/Graphs/MatrixBFS/shortestPath.py
+++ AandDSforBeginners/Graphs/MatrixBFS/shortestPath.py
@@ -2,24 +2,25 @@ class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
 
-        N = len(grid)
-        visit = set()
-        queue = deque([(0, 0, 1)])
-        visit.add((0, 0))
-        neighbors = [[0, 1], [0, -1], [1, 1], [1, -1], [-1, -1], [-1, 1], [1, 0], [-1, 0]]
+        q = deque()
+        seen = set()
+        q.append((0, 0, 1))
+        seen.add((0, 0))
 
-        while queue:
-            r, c, length = queue.popleft()
-            if (min(r, c) < 0 or
-            max(r, c) >= N or grid[r][c]):
+        neighbors = [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]]
+
+        while q:
+            r, c, length = q.popleft()
+
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] == 1:
                 continue
 
-            if r == N - 1 and c == N - 1:
+            if r == ROWS - 1 and c == COLS - 1:
                 return length
 
             for dr, dc in neighbors:
-                if (r + dr, c + dc) not in visit:
-                    queue.append((r + dr, c + dc, length + 1))
-                    visit.add((r + dr, c + dc))
+                if (r + dr, c + dc) not in seen:
+                    q.append((r + dr, c + dc, length + 1))
+                    seen.add((r + dr, c + dc))
 
         return -1
