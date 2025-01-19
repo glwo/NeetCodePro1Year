54a65ab0b8387f317b760609265f7f82d2bf25f9
glen <glenwojnar@yahoo.com>
2025-01-18 21:23:19 -0700

    Started dynamic programming

---
+++ AandDSforBeginners/DynamicProgramming/1-Dimension_DP/climbingStairs.py
@@ -0,0 +1,36 @@
+# No memo
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        def dfs(i):
+            if i >= n:
+                return i == n
+
+            return dfs(i + 1) + dfs(i + 2)
+
+        return dfs(0)
+
+# Top down memo
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        cache = [-1] * n
+        def dfs(i):
+            if i >= n:
+                return i == n
+            if cache[i] != -1:
+                return cache[i]
+            cache[i] = dfs(i + 1) + dfs(i + 2)
+            return cache[i]
+
+        return dfs(0)
+
+# Bottom up memo
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        dp = [0] * (n + 1)
+        dp[1], dp[2] = 1, 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
