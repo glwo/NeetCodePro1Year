d4176e242a2db1be57cc7b914f4df737713c3e41
glen <glenwojnar@yahoo.com>
2025-01-19 17:59:51 -0700

    first dynamic programming done

---
+++ AandDSforBeginners/DynamicProgramming/1-Dimension_DP/houseRobber.py
@@ -0,0 +1,43 @@
+# Recursion
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        def dfs(i):
+            if i >= len(nums):
+                return 0
+            return max(dfs(i + 1),
+                       nums[i] + dfs(i + 2))
+
+        return dfs(0)
+
+# Top down dynamic programming
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = [-1] * len(nums)
+
+        def dfs(i):
+            if i >= len(nums):
+                return 0
+            if memo[i] != -1:
+                return memo[i]
+            memo[i] = max(dfs(i + 1), nums[i] + dfs(i + 2))
+            return memo[i]
+
+        return dfs(0)
+
+# Bottom up dynamic programming
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
+
+        return dp[-1]
